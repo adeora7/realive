80e994b01dc3f5dfdd1b5b371861ac4734fe8062
adeora7 <deoraabhishek1995@gmail.com>
2015-06-28 19:00:39 +0530

    user and their campaigns correction

--- app/campaign/model.js
+++ app/campaign/model.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 var attr= DS.attr;
 
 var Campaigns= DS.Model.extend({
-	response: DS.hasMany('response'),
+	response: DS.hasMany('response', {async:true }),
 	user: DS.belongsTo('user'),
 	title: attr('string'),
 	end_date: attr('date'),
@@ -19,12 +19,12 @@ var Campaigns= DS.Model.extend({
 
 Campaigns.reopenClass({
 	FIXTURES: [
-	{id:1, user: 'adeora7', title: 'new post', city: 'allahabad',
+	{id:1, user: '1', title: 'new post', city: 'allahabad',
 	budget: '5000', locality_preference: 'IIITA',
 	hobbies: 'something',
 	priorities: 'something',
 	message: 'something'},
-	{id:2, user: 'adeora7', title: 'second post', city: 'jalandhar',
+	{id:2, user: '1', title: 'second post', city: 'jalandhar',
 	budget: '10000', locality_preference: 'MD',
 	hobbies: 'something more',
 	priorities: 'something more',
--- app/user/model.js
+++ app/user/model.js
@@ -3,8 +3,8 @@ import DS from 'ember-data';
 var attr= DS.attr;
 
 var User= DS.Model.extend({
-	response: DS.hasMany('response'),
-	campaign: DS.hasMany('campaign'),
+	response: DS.hasMany('response', { async:true }),
+	campaign: DS.hasMany('campaign', { async:true }),
 	name: attr('string'),
 	password: attr('string'),
   	age: attr('integer'),
@@ -15,7 +15,7 @@ var User= DS.Model.extend({
 User.reopenClass({
 	FIXTURES: [
 	{id:1, name: 'adeora7', password: 'password', age: '19',
-	gender: 'male', occupation: 'student'},
+	gender: 'male', occupation: 'student', campaign: [1,2] },
 	{id:2, name: 'anonymous', password: 'password', age: '20',
 	gender: 'male', occupation: 'student'}
 	]
